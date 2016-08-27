2fe102d99f52d8f91b3012d356b177fca5141efa
Amit Kumar Dubey <thedesignerkumar@gmail.com>
2016-08-27 18:59:20 +0530

    initial commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,1 @@
+TweenMax.to("#logo-outer",20,{rotation:180,transformOrigin:"50% 50%",repeat:-1,repeatDelay:0});
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');var gutil = require('gulp-util');
+
+gulp.task('scripts', function() {
+    gulp.src('dev/scripts/**/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/assets/js'))
+});
+
+gulp.task('styles', function() {
+	gulp.src('dev/scss/styles.scss')
+		.pipe(compass({
+			config_file: './config.rb',
+			css: 'app/assets/css',
+			sass: 'dev/sass'
+		}))
+        .pipe(gulp.dest('app/assets/css'))
+});
+
+gulp.task('images', function(){
+	gulp.src('dev/images/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('app/assets/images'))
+});
+
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true
+        }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('dev/sass/**/*.scss', ['styles']);
+    gulp.watch('dev/scripts/**/*.js', ['scripts']);
+    gulp.watch('dev/images/**/*', ['images']);
+});
+
+gulp.task('default', ['watch', 'styles', 'scripts', 'images', 'webserver']);
